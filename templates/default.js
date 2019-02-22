a03b4b6d9dbc37e0f566822a2fb098cc39580474
Jamis Charles <Jamis82@gmail.com>
2019-02-22 13:14:50 -0700

    Update default.js

--- templates/default.js
+++ templates/default.js
@@ -18,12 +18,16 @@ ${data.body}
 `;
 
     // FIXME: list example of output here...
-
     return template;
   },
   getOptions: function() {
     return {
-      folderForEachSlug: false,
+      folderForEachSlug: false, // same folder for all posts
+      imagePath: '/media', // <img src="/media/[filename]" >. Used in the markdown files.
+      // This field is ignored when folderForEachSlug:true. Should be absolute. Location where medium images will be saved.
+      imageFolder: '/Users/jacharles/dev/blog/static/media',
+      defaultCodeBlockLanguage: '', // code fenced by default will be ``` with no lang. If most of your code blocks are in a specific lang, set this here.
+
     };
   },
 };
